c950da04c3b2925012bc76a7d51ea55f46546ae6
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-03-12 18:55:00 +0100

    Improve build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,43 @@
 import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import { dirname } from 'path';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs',
-      exports: 'default',
-    },
-    { file: 'dist/index.esm.js', format: 'esm' },
-  ],
-  plugins: [typescript({ tsconfig: 'tsconfig.json' })],
-};
+function getOutput(format = 'esm') {
+  if (format === 'esm') {
+    return { dir: dirname(pkg.module), format };
+  }
+
+  return { file: pkg.main, format, exports: 'default' };
+}
+
+function getPlugins(format = 'esm') {
+  const typeScriptOptions =
+    format === 'esm'
+      ? { declaration: true, declarationDir: dirname(pkg.module) }
+      : {};
+
+  return [typescript(typeScriptOptions), terser()];
+}
+
+const input = 'src/index.ts';
+
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+export default [
+  {
+    input,
+    output: getOutput('cjs'),
+    plugins: getPlugins('cjs'),
+    external,
+  },
+  {
+    input,
+    output: getOutput('esm'),
+    plugins: getPlugins('esm'),
+    external,
+  },
+];
