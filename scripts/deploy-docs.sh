b8604ae7d3434c6b645ec8e21873c74e30ac340f
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-04-08 21:05:18 +0200

    Add script to deploy docs

---
+++ scripts/deploy-docs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git checkout $1
+npm run docs
+aws s3 sync $1/ s3://ape.const.sh/$1 --profile ape.const.sh --region eu-west-1
+aws cloudfront create-invalidation --distribution-id E2DJVTI57GD63Z --paths '/$1/*' --profile ape.const.sh --region eu-west-1
