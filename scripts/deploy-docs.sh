fb6f33d024dbdfb640134bb84a89306f562ed500
Florian Eckerstorfer <florian@eckerstorfer.net>
2021-04-08 21:06:39 +0200

    Fix local path in deploy-docs script

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -2,5 +2,5 @@
 
 git checkout $1
 npm run docs
-aws s3 sync $1/ s3://ape.const.sh/$1 --profile ape.const.sh --region eu-west-1
+aws s3 sync docs/ s3://ape.const.sh/$1 --profile ape.const.sh --region eu-west-1
 aws cloudfront create-invalidation --distribution-id E2DJVTI57GD63Z --paths '/$1/*' --profile ape.const.sh --region eu-west-1
